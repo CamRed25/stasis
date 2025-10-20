33b376f79066283e0e23a30deb02f31a305add60
CamRed25 <139036440+CamRed25@users.noreply.github.com>
2025-10-20 17:56:13 -0500

    fix: update flake.nix and flake.lock for improved rust-overlay integration

--- flake.nix
+++ flake.nix
@@ -3,48 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
+    rust-overlay,
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
-      system = system;
-      overlays = [
-        (import (builtins.fetchTarball {
-          url = "https://github.com/oxalica/rust-overlay/archive/refs/heads/master.tar.gz";
-          sha256 = "sha256:16d5wlabz1fydrh2hsh4vabidysh7ja9agx4d5sf79811j7fwf7r";
-        }))
-      ];
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
     };
 
     stasisSrc = pkgs.fetchFromGitHub {
       owner = "saltnpepper97";
       repo = "stasis";
-      rev = "main";
+      rev = "58876355050247a7bbab4c1f0bf50a15ccb81c3b";
       sha256 = "sha256-MOb56PJS5gBITScJMvou/Z6IGN/Xfw+f114v5Fxctf0=";
     };
-  in {
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+
+    myPkg = pkgs.rustPlatform.buildRustPackage {
       pname = "stasis";
-      version = "latest";
+      version = "0.4.12";
       src = stasisSrc;
       cargoHash = "sha256-M5L6kcx/FY+cusYhVSDoKCyuH0LpaPXzBo3wJZsLQak=";
-      nativeBuildInputs = [
-        pkgs.pkg-config
-        pkgs.openssl
-        pkgs.systemd
-        pkgs.dbus.dev
-        pkgs.libinput
-        pkgs.rust-bin.stable.latest.default
-      ];
-      buildInputs = [
-        pkgs.openssl
-        pkgs.systemd
-        pkgs.dbus.dev
-        pkgs.libinput
+      nativeBuildInputs = with pkgs; [pkg-config rust-bin.stable."1.89.0".default];
+      buildInputs = with pkgs; [openssl systemd dbus.dev libinput];
+    };
+  in {
+    packages.${system} = {
+      stasis = myPkg;
+      default = myPkg;
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rust-bin.stable."1.89.0".default
+        pkg-config
+        openssl
+        systemd
+        dbus.dev
+        libinput
       ];
     };
   };
